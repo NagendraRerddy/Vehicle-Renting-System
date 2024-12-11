ddaf85626917b11d27b94375d443b91c481ff4d4
NagendraReddy <pnagendrareddy793@gmail.com>
2024-12-11 23:56:13 +0530

    Project Created

---
+++ src/main/java/com/example/vehiclerentingsystem/VehiclerentingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehiclerentingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclerentingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclerentingsystemApplication.class, args);
+	}
+
+}
